06862c4fc4f1c81f7e98b7bcddb5d53988428077
David Brom <92022622+DavidBr27@users.noreply.github.com>
2024-02-26 23:09:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Initialization
 const calorieCounter = document.getElementById('calorie-counter');
 const budgetNumberInput = document.getElementById('budget');
 const entryDropdown = document.getElementById('entry-dropdown');
